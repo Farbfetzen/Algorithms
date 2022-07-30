6b982ab1c65b1ad75d8e58076cb9273b412ddfee
farbfetzen <bastihz.dev@posteo.de>
2022-07-31 00:57:34 +0200

    Extract Step as SortStep into separate file

--- Java/src/main/java/de/farbfetzen/algorithms/sorting/HeapSort.java
+++ Java/src/main/java/de/farbfetzen/algorithms/sorting/HeapSort.java
@@ -13,18 +13,8 @@ import static de.farbfetzen.algorithms.sorting.SortingUtils.swap;
 class HeapSort extends StepWiseSorter {
 
     private static final Set<Integer> CLEAR = Set.of();
-    private final List<Step> steps = new ArrayList<>();
-    private final Iterator<Step> stepIterator;
-
-    /**
-     * One step that was performed and recorded during the sorting of the array.
-     * The visualization works by replaying these steps.
-     * @param swap the swapped indices or null if no swap happened
-     * @param comparisons the compared indices or null if nothing was compared
-     * @param highlights the highlighted indices or null if nothing was highlighted
-     */
-    @SuppressWarnings("squid:S6218")
-    private record Step(int[] swap, Set<Integer> comparisons, Set<Integer> highlights) {}
+    private final List<SortStep> steps = new ArrayList<>();
+    private final Iterator<SortStep> stepIterator;
 
     HeapSort(final int[] array) {
         super(array);
@@ -36,14 +26,14 @@ class HeapSort extends StepWiseSorter {
     void step() {
         if (stepIterator.hasNext()) {
             final var step = stepIterator.next();
-            if (step.swap != null) {
-                swap(array, step.swap[0], step.swap[1]);
+            if (step.swap() != null) {
+                swap(array, step.swap()[0], step.swap()[1]);
             }
-            if (step.comparisons != null) {
-                comparisons = step.comparisons;
+            if (step.comparisons() != null) {
+                comparisons = step.comparisons();
             }
-            if (step.highlights != null) {
-                highlights = step.highlights;
+            if (step.highlights() != null) {
+                highlights = step.highlights();
             }
         } else {
             finished = true;
@@ -53,7 +43,7 @@ class HeapSort extends StepWiseSorter {
     private void sortAndRecord(final int[] array) {
         buildHeapAndRecord(array);
         for (int i = array.length - 1; i >= 1; i--) {
-            steps.add(new Step(new int[]{0, i}, null, Set.of(i)));
+            steps.add(new SortStep(new int[]{0, i}, null, Set.of(i)));
             swap(array, 0, i);
             heapifyAndRecord(array, 0, i);
         }
@@ -61,7 +51,7 @@ class HeapSort extends StepWiseSorter {
 
     private void buildHeapAndRecord(final int[] array) {
         for (int i = array.length / 2 - 1; i >= 0; i--) {
-            steps.add(new Step(null, CLEAR, Set.of(i)));
+            steps.add(new SortStep(null, CLEAR, Set.of(i)));
             heapifyAndRecord(array, i, array.length);
         }
     }
@@ -71,19 +61,19 @@ class HeapSort extends StepWiseSorter {
         final var left = i * 2 + 1;
         final var right = i * 2 + 2;
         if (left < upperBound) {
-            steps.add(new Step(null, Set.of(largest, left), null));
+            steps.add(new SortStep(null, Set.of(largest, left), null));
             if (array[largest] < array[left]) {
                 largest = left;
             }
         }
         if (right < upperBound) {
-            steps.add(new Step(null, Set.of(largest, right), null));
+            steps.add(new SortStep(null, Set.of(largest, right), null));
             if (array[largest] < array[right]) {
                 largest = right;
             }
         }
         if (largest != i) {
-            steps.add(new Step(new int[]{largest, i}, CLEAR, null));
+            steps.add(new SortStep(new int[]{largest, i}, CLEAR, null));
             swap(array, largest, i);
             heapifyAndRecord(array, largest, upperBound);
         }
